465bdec6346425e9682acb28608c7fc576f42688
GIKontsevik <85735179+GIKontsevik@users.noreply.github.com>
2021-06-15 11:59:23 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,23 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 def fib_num(i, calculated = {1:0, 2:1}):
     if i not in calculated:
         calculated[i] = fib_num(i-1, calculated) + fib_num(i-2, calculated)
     return calculated[i]
 
+#fib_num(10)
 
-# In[ ]:
-
-
-fib_num(10)
-
-
-# In[ ]:
-
-
-get_ipython().run_line_magic('timeit', 'fib_num(10)')
+#get_ipython().run_line_magic('timeit', 'fib_num(10)')
 
